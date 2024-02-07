55cb37f7dbe922c97edfd94b93c9ec1b94f30ea1
martinfosby <marfos19@gmail.com>
2024-02-07 19:41:25 +0100

    First working version, only works when having february as input

--- app/src/main/java/com/example/calendarapp/MainActivity.kt
+++ app/src/main/java/com/example/calendarapp/MainActivity.kt
@@ -3,46 +3,192 @@ package com.example.calendarapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calendarapp.ui.theme.CalendarAppTheme
+import com.example.calendarapp.utils.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalendarAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Calendar()
+                Column {
+                    var yearInput by remember { mutableStateOf("2024") }
+                    var monthInput by remember { mutableStateOf("2") }
+
+                    TextField(
+                        value = yearInput,
+                        onValueChange = {
+                            yearInput = it
+                        }
+                    )
+                    TextField(
+                        value = monthInput,
+                        onValueChange = {
+                            monthInput = it
+                        }
+                    )
+                    val year = yearInput.toIntOrNull() ?: 2024 // Use 2024 as default if input is empty or not a valid number
+                    val month = monthInput.toIntOrNull() ?: 2 // Use 2 as default if input is empty or not a valid number
+                    val monthInfo = MonthInfo(month)
+                    Calendar(year, monthInfo)
                 }
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Calendar(modifier: Modifier = Modifier) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Februar")
-        Row {
-            Text(text = "M")
-            Text(text = "T")
-            Text(text = "O")
-            Text(text = "T")
-            Text(text = "F")
-            Text(text = "L")
-            Text(text = "S")
+fun Calendar(year: Int, month: MonthInfo, modifier: Modifier = Modifier.fillMaxSize()) {
+    val daysIndex = arrayOf("M", "T", "O", "T", "F", "L", "S")
+    val weekOfMonth = getWeeksInMonth(year, month.index)
+    val daysOfMonth = getDaysOfMonth(year, month.index)
+    var daysSinceFirstJan by remember {
+        mutableIntStateOf(0)
+    }
+    var currentDay by remember {
+        mutableIntStateOf(0)
+    }
+    var workingDays = numberOfWorkDays(year, month.index)
+
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = month.name,
+                    fontWeight = FontWeight.Bold
+                )
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        )
+    }, bottomBar = {
+        BottomAppBar(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.White
+        ) {
+
+          
+            // Add more icons or content here if needed
+        }
+    }) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            Box(
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.androidparty), // Background image
+                    contentDescription = null, // Content description for accessibility
+                    contentScale = ContentScale.FillBounds, // Adjust content scale if needed
+                    alpha = 0.5F
+                )
+
+
+
+                Column {
+                    Row {
+                        Box(modifier = Modifier.padding(20.dp))
+                        LazyVerticalGrid(columns = GridCells.Fixed(7), modifier = Modifier) {
+                            items(daysIndex) { dayName ->
+                                if (dayName == "S")
+                                    Text(text = dayName, fontSize = 35.sp, color = Color.Red)
+                                else
+                                    Text(text = dayName, fontSize = 35.sp)
+                            }
+                        }
+                    }
+                    Row {
+                        Column(
+                            modifier = Modifier.padding(end = 20.dp)
+                        ) {
+                            weekOfMonth.forEach {
+                                Text(text = "$it", fontSize = 30.sp)
+                            }
+                        }
+                        Column {
+                            LazyVerticalGrid(columns = GridCells.Fixed(7), modifier = Modifier) {
+                                items(daysOfMonth) { day ->
+                                    // Display each day of the month as a separate item
+                                    // You can customize the UI for each day here
+
+                                    Text(
+                                        text = day.toString(),
+                                        fontSize = 30.sp,
+                                        modifier = Modifier.clickable {
+                                            daysSinceFirstJan = numberOfDaysSinceFirstJan(year, month.index, day)
+                                            currentDay = if (day != " ") day as Int else 0
+                                        }
+                                    )
+
+                                }
+                            }
+                            Column(
+                                modifier = Modifier
+                                    .padding(40.dp)
+                                    .background(MaterialTheme.colorScheme.background)
+                            ) {
+                                Text(
+                                    text = "Denne måneden har $workingDays arbeidsdager.",
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 20.sp,
+                                    modifier = Modifier.padding(8.dp)
+                                )
+                                Text(
+                                    text = if (daysSinceFirstJan == 0) "Klikk på en dag for å finne antall dager siden 1. januar."
+                                    else "Dager siden 1 januar for dag $currentDay er: $daysSinceFirstJan dager.",
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 20.sp,
+                                    modifier = Modifier.padding(8.dp)
+                                )
+                            }
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
@@ -51,6 +197,9 @@ fun Calendar(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CalendarAppTheme {
-        Calendar()
+        Calendar(2024, MonthInfo(2))
     }
 }
+
+
+
---
+++ app/src/main/java/com/example/calendarapp/utils/HelperFunctions.kt
@@ -0,0 +1,120 @@
+package com.example.calendarapp.utils
+
+import java.util.Calendar
+
+
+data class MonthInfo(val index: Int){
+    val name: String
+        get() = when(index) {
+            1 -> "Januar"
+            2 -> "Februar"
+            3 -> "Mars"
+            4 -> "April"
+            5 -> "Mai"
+            6 -> "Juni"
+            7 -> "Juli"
+            8 -> "August"
+            9 -> "September"
+            10 -> "Oktober"
+            11 -> "November"
+            12 -> "Desember"
+            else -> "Ukjent"
+        }
+
+
+
+}
+
+
+fun getDaysInMonth(year: Int, month: Int): Int {
+    val calendar = Calendar.getInstance()
+    calendar.set(Calendar.YEAR, year)
+    calendar.set(Calendar.MONTH, month - 1) // Calendar months are zero-based
+    return calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+}
+
+fun getWeeksInMonth(year: Int, month: Int): List<Int> {
+    val calendar = Calendar.getInstance()
+    calendar.firstDayOfWeek = Calendar.MONDAY // Set Monday as the first day of the week
+    calendar.set(Calendar.YEAR, year)
+    calendar.set(Calendar.MONTH, month - 1) // Calendar months are zero-based
+    var startDay = 1
+    calendar.set(Calendar.DAY_OF_MONTH, startDay)
+
+    val weekNumbers = mutableListOf<Int>()
+
+    val lastDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+
+    while (calendar.get(Calendar.DAY_OF_MONTH) <= lastDayOfMonth) {
+        val weekNumber = calendar.get(Calendar.WEEK_OF_YEAR)
+        if (weekNumber !in weekNumbers) {
+            weekNumbers.add(weekNumber)
+        }
+        calendar.set(Calendar.DAY_OF_MONTH, ++startDay)
+    }
+
+    return weekNumbers
+}
+
+fun getDayOfWeek(year: Int, month: Int, dayOfMonth: Int): Int {
+    val calendar = Calendar.getInstance()
+    calendar.firstDayOfWeek = Calendar.MONDAY // Set Monday as the first day of the week
+    calendar.set(Calendar.YEAR, year)
+    calendar.set(Calendar.MONTH, month - 1) // Calendar months are zero-based
+    calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+    return calendar.get(Calendar.DAY_OF_WEEK) - 1
+}
+fun getDaysOfMonth(year: Int, month: Int): List<Any> {
+    val dayOfWeek = getDayOfWeek(year, month, 1)
+    val daysOfMonth = getDaysInMonth(year, month)
+
+    // Create a list representing the days of the month
+    val days = mutableListOf<Any>()
+
+    // Add empty spaces for the days before the first day of the month
+    for (i in 1 until dayOfWeek) {
+        days.add(" ")
+    }
+
+    // Add the actual days of the month
+    for (day in 1..daysOfMonth) {
+        days.add(day)
+    }
+
+    return days
+}
+
+
+fun numberOfDaysSinceFirstJan(year: Int, month: Int, dayOfMonth: Any): Int {
+    if (dayOfMonth != " ") {
+        val calendar = Calendar.getInstance()
+        calendar.firstDayOfWeek = Calendar.MONDAY // Set Monday as the first day of the week
+        calendar.set(Calendar.YEAR, year)
+        calendar.set(Calendar.MONTH, month - 1) // Calendar months are zero-based
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth as Int)
+
+        return calendar.get(Calendar.DAY_OF_YEAR) - 1
+    }
+    else return 0
+}
+
+fun numberOfWorkDays(year: Int, month: Int): Int {
+    val calendar = Calendar.getInstance()
+    calendar.firstDayOfWeek = Calendar.MONDAY // Set Monday as the first day of the week
+    calendar.set(Calendar.YEAR, year)
+    calendar.set(Calendar.MONTH, month - 1) // Calendar months are zero-based
+
+    val daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+    var workingDays = 0
+
+    for (day in 1..daysInMonth) {
+        calendar.set(Calendar.DAY_OF_MONTH, day)
+        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+        if (dayOfWeek != Calendar.SATURDAY && dayOfWeek != Calendar.SUNDAY) {
+            workingDays++
+        }
+    }
+
+    return workingDays
+}
